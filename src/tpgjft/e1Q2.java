008c41ffa2c876336d9eff7982daa46e28d59a6a
Hantus <hantusrossouw@yahoo.com>
2022-06-01 19:15:30 +0200

    Ohs-12 initial commit

---
+++ src/tpgjft/e1Q2.java
@@ -0,0 +1,12 @@
+package tpgjft;
+
+public class e1Q2 {
+    public static void main(String[] args) {
+        int num1 = 90;
+        double num2 = 90.77;
+        String str1 = "I like Automation";
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(str1);
+    }
+}
